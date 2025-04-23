fd449b5fb9b0a5c6a6796d2ef64fb20aeb7d0e74
123 <a2511069202@163.com>
2025-04-23 17:04:14 +0800

    add force has_b_frame==0

--- libavcodec/h264_slice.c
+++ libavcodec/h264_slice.c
@@ -1646,6 +1646,7 @@ static int h264_select_output_frame(H264Context *h)
             out = h->delayed_pic[i];
             out_idx = i;
         }
+    h->avctx->has_b_frames = 0;
     if (h->avctx->has_b_frames == 0 &&
         ((h->delayed_pic[0]->f->flags & AV_FRAME_FLAG_KEY) || h->delayed_pic[0]->mmco_reset))
         h->next_outputed_poc = INT_MIN;
